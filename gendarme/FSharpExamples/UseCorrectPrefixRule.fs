591a4a910a1f116a5962c3a93b77ab0d94cb6da1
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-02-15 14:19:00 +0000

    Another cornucopia of spurious warnings

---
+++ gendarme/FSharpExamples/UseCorrectPrefixRule.fs
@@ -0,0 +1,33 @@
+﻿namespace UseCorrectPrefix
+
+open System
+
+type Params =
+  { /// Path to the Altcover executable.
+    ToolPath : string
+    /// Working directory for relative file paths.  Default is the current working directory
+    WorkingDirectory : string }
+
+  static member Create() =
+    { ToolPath = "altcover"
+      WorkingDirectory = String.Empty }
+
+module CreateProcess =
+  let ensureExitCode = id
+
+
+module FSApi =
+
+    let createProcess (parameters:Params)  =
+      let doTool() =
+        parameters.ToolPath
+
+      let withWorkingDirectory c =
+        c
+        |> if String.IsNullOrWhiteSpace parameters.WorkingDirectory
+           then id
+           else (fun s -> sprintf "(%s)%s" parameters.WorkingDirectory c)
+
+      doTool()
+      |> withWorkingDirectory
+      |> CreateProcess.ensureExitCode
