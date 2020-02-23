cfa0d3760d44bf859a7da84bcc07213f75a89cda
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-02-23 14:04:51 +0000

    Fix first changed test

--- gendarme/rules/Test.Rules/Fixtures/MethodRuleTestFixture.cs
+++ gendarme/rules/Test.Rules/Fixtures/MethodRuleTestFixture.cs
@@ -28,6 +28,7 @@
 //
 
 using System;
+using System.Linq;
 
 using Gendarme.Framework;
 using Gendarme.Framework.Rocks;
@@ -35,158 +36,164 @@ using Test.Rules.Helpers;
 
 using Mono.Cecil;
 
-namespace Test.Rules.Fixtures {
-	
-	/// <summary>
-	/// Abstract class providing various helper methods that method test fixtures should inherit from.
-	/// </summary>
-	/// <typeparam name="TMethodRule">Type of rule to be tested.</typeparam>
-	public abstract class MethodRuleTestFixture<TMethodRule> : RuleTestFixture<TMethodRule, MethodDefinition>
-		where TMethodRule : IMethodRule, new () {
+namespace Test.Rules.Fixtures
+{
+  /// <summary>
+  /// Abstract class providing various helper methods that method test fixtures should inherit from.
+  /// </summary>
+  /// <typeparam name="TMethodRule">Type of rule to be tested.</typeparam>
+  public abstract class MethodRuleTestFixture<TMethodRule> : RuleTestFixture<TMethodRule, MethodDefinition>
+    where TMethodRule : IMethodRule, new()
+  {
+    /// <summary>
+    /// Asserts that the rule does not apply to all methods of the type.
+    /// </summary>
+    /// <typeparam name="T">Type containing the methods.</typeparam>
+    protected void AssertRuleDoesNotApply<T>()
+    {
+      foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T>().Methods)
+        base.AssertRuleDoesNotApply(method);
+    }
 
-		/// <summary>
-		/// Asserts that the rule does not apply to all methods of the type. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the methods.</typeparam>
-		protected void AssertRuleDoesNotApply<T> ()
-		{
-			foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T> ().Methods)
-				base.AssertRuleDoesNotApply (method);
-		}
-		
-		/// <summary>
-		/// Asserts that the rule does not apply to the method. 
-		/// </summary>
-		/// <param name="method">Method to check.</param>
-		/// <typeparam name="T">Type containing the method.</typeparam>
-		protected void AssertRuleDoesNotApply<T> (string method)
-		{
-			base.AssertRuleDoesNotApply (DefinitionLoader.GetMethodDefinition<T> (method));
-		}
-		
-		/// <summary>
-		/// Asserts that the rule does not apply to the method. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		/// <param name="parameters">Parameter types.</param>
-		protected void AssertRuleDoesNotApply<T> (string method, Type [] parameters)
-		{
-			base.AssertRuleDoesNotApply (DefinitionLoader.GetMethodDefinition<T> (method, parameters));
-		}
+    /// <summary>
+    /// Asserts that the rule does not apply to the method.
+    /// </summary>
+    /// <param name="method">Method to check.</param>
+    /// <typeparam name="T">Type containing the method.</typeparam>
+    protected void AssertRuleDoesNotApply<T>(string method)
+    {
+      base.AssertRuleDoesNotApply(DefinitionLoader.GetMethodDefinition<T>(method));
+    }
 
-		protected void AssertRuleDoesNotApply (Type type, string method)
-		{
-			TypeDefinition td = DefinitionLoader.GetTypeDefinition (type);
-			base.AssertRuleDoesNotApply (DefinitionLoader.GetMethodDefinition (td, method, null));
-		}
-		
-		/// <summary>
-		/// Asserts that the rule has been executed successfully for each method in the type. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the methods.</typeparam>
-		protected void AssertRuleSuccess<T> ()
-		{
-			foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T> ().Methods)
-				base.AssertRuleSuccess (method);
-		}
-		
-		/// <summary>
-		/// Asserts that the rule has been executed successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		protected void AssertRuleSuccess<T> (string method)
-		{
+    /// <summary>
+    /// Asserts that the rule does not apply to the method.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    /// <param name="parameters">Parameter types.</param>
+    protected void AssertRuleDoesNotApply<T>(string method, Type[] parameters)
+    {
+      base.AssertRuleDoesNotApply(DefinitionLoader.GetMethodDefinition<T>(method, parameters));
+    }
+
+    protected void AssertRuleDoesNotApply(Type type, string method)
+    {
+      TypeDefinition td = DefinitionLoader.GetTypeDefinition(type);
+      base.AssertRuleDoesNotApply(DefinitionLoader.GetMethodDefinition(td, method, null));
+    }
+
+    /// <summary>
+    /// Asserts that the rule has been executed successfully for each method in the type.
+    /// </summary>
+    /// <typeparam name="T">Type containing the methods.</typeparam>
+    protected void AssertRuleSuccess<T>()
+    {
+      AssertRuleSuccess<T>(x => x != null);
+    }
+
+    protected void AssertRuleSuccess<T>(Func<MethodDefinition, bool> predicate)
+    {
+      foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T>().Methods
+                  .Where(predicate))
+        base.AssertRuleSuccess(method);
+    }
+
+    /// <summary>
+    /// Asserts that the rule has been executed successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    protected void AssertRuleSuccess<T>(string method)
+    {
 			base.AssertRuleSuccess (DefinitionLoader.GetMethodDefinition<T> (method));
-		}
+    }
 
-		/// <summary>		
-		/// Asserts that the rule has been executed successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		/// <param name="parameters">Parameter types.</param>
-		protected void AssertRuleSuccess<T> (string method, Type [] parameters)
-		{
-			base.AssertRuleSuccess (DefinitionLoader.GetMethodDefinition<T> (method, parameters));
-		}
+    /// <summary>
+    /// Asserts that the rule has been executed successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    /// <param name="parameters">Parameter types.</param>
+    protected void AssertRuleSuccess<T>(string method, Type[] parameters)
+    {
+      base.AssertRuleSuccess(DefinitionLoader.GetMethodDefinition<T>(method, parameters));
+    }
 
-		protected void AssertRuleSuccess (Type type, string method)
-		{
-			TypeDefinition td = DefinitionLoader.GetTypeDefinition (type);
-			base.AssertRuleSuccess (DefinitionLoader.GetMethodDefinition (td, method, null));
-		}
+    protected void AssertRuleSuccess(Type type, string method)
+    {
+      TypeDefinition td = DefinitionLoader.GetTypeDefinition(type);
+      base.AssertRuleSuccess(DefinitionLoader.GetMethodDefinition(td, method, null));
+    }
 
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully for each method in the type. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the methods.</typeparam>
-		protected void AssertRuleFailure<T> ()
-		{
-			foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T> ().Methods)
-				base.AssertRuleFailure (method);
-		}
-		
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully for each method in the type. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the methods.</typeparam>
-		/// <param name="expectedCount">Expected defect count for each method.</param>
-		protected void AssertRuleFailure<T> (int expectedCount)
-		{
-			foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T> ().Methods)
-				base.AssertRuleFailure (method, expectedCount);
-		}
-		
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		protected void AssertRuleFailure<T> (string method)
-		{
-			base.AssertRuleFailure (DefinitionLoader.GetMethodDefinition<T> (method));
-		}
-		
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		/// <param name="expectedCount">Expected message count.</param>
-		protected void AssertRuleFailure<T> (string method, int expectedCount)
-		{
-			base.AssertRuleFailure (DefinitionLoader.GetMethodDefinition<T> (method), expectedCount);
-		}
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully for each method in the type.
+    /// </summary>
+    /// <typeparam name="T">Type containing the methods.</typeparam>
+    protected void AssertRuleFailure<T>()
+    {
+      foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T>().Methods)
+        base.AssertRuleFailure(method);
+    }
 
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		/// <param name="parameters">Parameter types.</param>
-		protected void AssertRuleFailure<T> (string method, Type [] parameters)
-		{
-			base.AssertRuleFailure (DefinitionLoader.GetMethodDefinition<T> (method, parameters));
-		}
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully for each method in the type.
+    /// </summary>
+    /// <typeparam name="T">Type containing the methods.</typeparam>
+    /// <param name="expectedCount">Expected defect count for each method.</param>
+    protected void AssertRuleFailure<T>(int expectedCount)
+    {
+      foreach (MethodDefinition method in DefinitionLoader.GetTypeDefinition<T>().Methods)
+        base.AssertRuleFailure(method, expectedCount);
+    }
 
-		/// <summary>
-		/// Asserts that the rule has failed to execute successfully. 
-		/// </summary>
-		/// <typeparam name="T">Type containing the method to test.</typeparam>
-		/// <param name="method">Method name.</param>
-		/// <param name="parameters">Parameter types.</param>
-		/// <param name="expectedCount">Expected message count.</param>
-		protected void AssertRuleFailure<T> (string method, Type [] parameters, int expectedCount)
-		{
-			base.AssertRuleFailure (DefinitionLoader.GetMethodDefinition<T> (method, parameters), expectedCount);
-		}
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    protected void AssertRuleFailure<T>(string method)
+    {
+      base.AssertRuleFailure(DefinitionLoader.GetMethodDefinition<T>(method));
+    }
 
-		protected void AssertRuleFailure (Type type, string method, int expectedCount)
-		{
-			TypeDefinition td = DefinitionLoader.GetTypeDefinition (type);
-			base.AssertRuleFailure (DefinitionLoader.GetMethodDefinition (td, method, null), expectedCount);
-		}
-	}
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    /// <param name="expectedCount">Expected message count.</param>
+    protected void AssertRuleFailure<T>(string method, int expectedCount)
+    {
+      base.AssertRuleFailure(DefinitionLoader.GetMethodDefinition<T>(method), expectedCount);
+    }
+
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    /// <param name="parameters">Parameter types.</param>
+    protected void AssertRuleFailure<T>(string method, Type[] parameters)
+    {
+      base.AssertRuleFailure(DefinitionLoader.GetMethodDefinition<T>(method, parameters));
+    }
+
+    /// <summary>
+    /// Asserts that the rule has failed to execute successfully.
+    /// </summary>
+    /// <typeparam name="T">Type containing the method to test.</typeparam>
+    /// <param name="method">Method name.</param>
+    /// <param name="parameters">Parameter types.</param>
+    /// <param name="expectedCount">Expected message count.</param>
+    protected void AssertRuleFailure<T>(string method, Type[] parameters, int expectedCount)
+    {
+      base.AssertRuleFailure(DefinitionLoader.GetMethodDefinition<T>(method, parameters), expectedCount);
+    }
+
+    protected void AssertRuleFailure(Type type, string method, int expectedCount)
+    {
+      TypeDefinition td = DefinitionLoader.GetTypeDefinition(type);
+      base.AssertRuleFailure(DefinitionLoader.GetMethodDefinition(td, method, null), expectedCount);
+    }
+  }
 }
