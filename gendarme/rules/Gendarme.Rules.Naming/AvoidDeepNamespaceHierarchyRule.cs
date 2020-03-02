803061ebf12f140d7441475b98cd9019a98929e8
Steve Gilham <SteveGilham@users.noreply.github.com>
2020-03-02 14:30:14 +0000

    Ignore F# generated `<StartuppCode$` namespaces

--- gendarme/rules/Gendarme.Rules.Naming/AvoidDeepNamespaceHierarchyRule.cs
+++ gendarme/rules/Gendarme.Rules.Naming/AvoidDeepNamespaceHierarchyRule.cs
@@ -35,127 +35,142 @@ using Gendarme.Framework;
 using Gendarme.Framework.Engines;
 using Gendarme.Framework.Helpers;
 
-namespace Gendarme.Rules.Naming {
+namespace Gendarme.Rules.Naming
+{
+  /// <summary>
+  /// This rule checks for deeply nested namespaces within an assembly. It will
+  /// warn if the depth is greater than four (default value) unless the fifth (or the
+  /// next) part is one of the specialized name that the framework recommends or a
+  /// name like an internal namespace (something not meant to be seen outside the assembly).
+  /// <list>
+  /// <item><term>Design</term><description>Namespace that provides design-time
+  /// support for its base namespace.</description></item>
+  /// <item><term>Interop</term><description>Namespace that provides all interoperability
+  /// code (e.g. p./invokes) for its base namespace.</description></item>
+  /// <item><term>Permissions</term><description>Namespace that provides all custom
+  /// permissions for its base namespace.</description></item>
+  /// <item><term>Internal</term><description>Namespace that provides non visible (outside
+  /// the assembly) helper code for its base namespace. <c>Impl</c> while allowed by the
+  /// rule is not encouraged.
+  /// </description></item>
+  /// </list>
+  /// </summary>
+  /// <example>
+  /// Bad example:
+  /// <code>
+  /// namespace One.Two.Three.Four.Five {
+  ///	internal class Helper {
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
+  /// <example>
+  /// Good example:
+  /// <code>
+  /// namespace One.Two.Three.Four {
+  ///	internal class FiveHelper {
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
+  /// <example>
+  /// Good example (exception for some namespace specialization):
+  /// <code>
+  /// namespace One.Two.Three.Four.Internal {
+  ///	internal class Helper {
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
 
-	/// <summary>
-	/// This rule checks for deeply nested namespaces within an assembly. It will
-	/// warn if the depth is greater than four (default value) unless the fifth (or the
-	/// next) part is one of the specialized name that the framework recommends or a
-	/// name like an internal namespace (something not meant to be seen outside the assembly).
-	/// <list>
-	/// <item><term>Design</term><description>Namespace that provides design-time
-	/// support for its base namespace.</description></item>
-	/// <item><term>Interop</term><description>Namespace that provides all interoperability
-	/// code (e.g. p./invokes) for its base namespace.</description></item>
-	/// <item><term>Permissions</term><description>Namespace that provides all custom 
-	/// permissions for its base namespace.</description></item>
-	/// <item><term>Internal</term><description>Namespace that provides non visible (outside
-	/// the assembly) helper code for its base namespace. <c>Impl</c> while allowed by the 
-	/// rule is not encouraged.
-	/// </description></item>
-	/// </list>
-	/// </summary>
-	/// <example>
-	/// Bad example:
-	/// <code>
-	/// namespace One.Two.Three.Four.Five {
-	///	internal class Helper {
-	///	}
-	/// }
-	/// </code>
-	/// </example>
-	/// <example>
-	/// Good example:
-	/// <code>
-	/// namespace One.Two.Three.Four {
-	///	internal class FiveHelper {
-	///	}
-	/// }
-	/// </code>
-	/// </example>
-	/// <example>
-	/// Good example (exception for some namespace specialization):
-	/// <code>
-	/// namespace One.Two.Three.Four.Internal {
-	///	internal class Helper {
-	///	}
-	/// }
-	/// </code>
-	/// </example>
+  [Problem("The depth of the namespace hierarchy is getting out of control.")]
+  [Solution("Try to keep the depth below 4, with an additional one for specialization (e.g. Design, Interop, Permissions).")]
+  [EngineDependency(typeof(NamespaceEngine))]
+  public class AvoidDeepNamespaceHierarchyRule : Rule, IAssemblyRule
+  {
+    private const int DefaultMaxDepth = 4;
+    private int max_depth = DefaultMaxDepth;
 
-	[Problem ("The depth of the namespace hierarchy is getting out of control.")]
-	[Solution ("Try to keep the depth below 4, with an additional one for specialization (e.g. Design, Interop, Permissions).")]
-	[EngineDependency (typeof (NamespaceEngine))]
-	public class AvoidDeepNamespaceHierarchyRule : Rule, IAssemblyRule {
+    /// <summary>The depth at which namespaces may be nested without triggering a defect.</summary>
+    /// <remarks>Defaults to 4.</remarks>
+    [DefaultValue(DefaultMaxDepth)]
+    [Description("The depth at which namespaces may be nested without triggering a defect.")]
+    public int MaxDepth
+    {
+      get
+      {
+        return max_depth;
+      }
+      set
+      {
+        if (value < 1)
+          throw new ArgumentOutOfRangeException("MaxDepth", "Minimum: 1");
+        max_depth = value;
+      }
+    }
 
-		private const int DefaultMaxDepth = 4;
-		private int max_depth = DefaultMaxDepth;
+    private static int CountLevels(string ns)
+    {
+      if (ns.Length == 0)
+        return 0;
 
-		/// <summary>The depth at which namespaces may be nested without triggering a defect.</summary>
-		/// <remarks>Defaults to 4.</remarks>
-		[DefaultValue (DefaultMaxDepth)]
-		[Description ("The depth at which namespaces may be nested without triggering a defect.")]
-		public int MaxDepth {
-			get {
-				return max_depth;
-			}
-			set {
-				if (value < 1)
-					throw new ArgumentOutOfRangeException ("MaxDepth", "Minimum: 1");
-				max_depth = value;
-			}
-		}
+      int levels = 0;
+      int index = 0;
+      while (index != -1)
+      {
+        index = ns.IndexOf('.', index + 1);
+        levels++;
+      }
+      return levels;
+    }
 
-		private static int CountLevels (string ns)
-		{
-			if (ns.Length == 0)
-				return 0;
+    public RuleResult CheckAssembly(AssemblyDefinition assembly)
+    {
+      // check every namespaces inside the assembly using the NamespaceEngine
+      foreach (string ns in NamespaceEngine.NamespacesInside(assembly))
+      {
+        // shortest invalid namespace would be "a.b.c.d.e"
+        // so we can skip anything less than 2 * MaxDepth
+        // note: overflow does not matter
+        if (ns.Length < unchecked(MaxDepth + MaxDepth))
+          continue;
 
-			int levels = 0;
-			int index = 0;
-			while (index != -1) {
-				index = ns.IndexOf ('.', index + 1);
-				levels++;
-			}
-			return levels;
-		}
+        // Ignore F# generated names like "<StartupCode$a-b-c-d>.$.NETFramework,Version=v4.7.2"
+        if (ns.StartsWith("<StartupCode$", StringComparison.Ordinal))
+          continue;
 
-		public RuleResult CheckAssembly (AssemblyDefinition assembly)
-		{
-			// check every namespaces inside the assembly using the NamespaceEngine
-			foreach (string ns in NamespaceEngine.NamespacesInside (assembly)) {
-				// shortest invalid namespace would be "a.b.c.d.e"
-				// so we can skip anything less than 2 * MaxDepth
-				// note: overflow does not matter
-				if (ns.Length < unchecked (MaxDepth + MaxDepth))
-					continue;
+        // count the levels (i.e. number of dots + 1)
+        int levels = CountLevels(ns);
+        if (levels <= MaxDepth)
+          continue;
 
-				// count the levels (i.e. number of dots + 1)
-				int levels = CountLevels (ns);
-				if (levels <= MaxDepth)
-					continue;
+        // we have some exceptions for namespace specialization
+        // i.e. stuff we often prefer to have in a sub-namespace
+        // and for internal (non-visible) namespaces
+        if (levels == MaxDepth + 1)
+        {
+          if (NamespaceDefinition.IsSpecialized(ns))
+          {
+            continue;
+          }
+          else if (ns.EndsWith(".Internal", StringComparison.Ordinal) || ns.EndsWith(".Impl", StringComparison.Ordinal))
+          {
+            continue;
+          }
+        }
 
-				// we have some exceptions for namespace specialization
-				// i.e. stuff we often prefer to have in a sub-namespace
-				// and for internal (non-visible) namespaces
-				if (levels == MaxDepth + 1) {
-					if (NamespaceDefinition.IsSpecialized (ns)) {
-						continue;
-					} else if (ns.EndsWith (".Internal", StringComparison.Ordinal) || ns.EndsWith (".Impl", StringComparison.Ordinal)) {
-						continue;
-					}
-				}
+        Severity severity = (levels < MaxDepth * 2) ? Severity.Medium : Severity.High;
+        Runner.Report(NamespaceDefinition.GetDefinition(ns), severity, Confidence.High);
+      }
+      return Runner.CurrentRuleResult;
+    }
 
-				Severity severity = (levels < MaxDepth * 2) ? Severity.Medium : Severity.High;
-				Runner.Report (NamespaceDefinition.GetDefinition (ns), severity, Confidence.High);
-			}
-			return Runner.CurrentRuleResult;
-		}
-
-		public override string Solution {
-			get {
-				return string.Format("Try to keep the depth below {0}, with an additional one for specialization (e.g. Design, Interop, Permissions).", MaxDepth);
-			}
-		}
-	}
+    public override string Solution
+    {
+      get
+      {
+        return string.Format("Try to keep the depth below {0}, with an additional one for specialization (e.g. Design, Interop, Permissions).", MaxDepth);
+      }
+    }
+  }
 }
