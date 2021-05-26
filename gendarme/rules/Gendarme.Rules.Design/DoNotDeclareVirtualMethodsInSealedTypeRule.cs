92211aa640499cf4004d67aaf2421cb67ee40243
Steve Gilham <SteveGilham@users.noreply.github.com>
2021-05-26 20:42:00 +0100

    Throw up our hands at the F# compiler

--- gendarme/rules/Gendarme.Rules.Design/DoNotDeclareVirtualMethodsInSealedTypeRule.cs
+++ gendarme/rules/Gendarme.Rules.Design/DoNotDeclareVirtualMethodsInSealedTypeRule.cs
@@ -31,59 +31,65 @@ using Mono.Cecil;
 using Gendarme.Framework;
 using Gendarme.Framework.Rocks;
 
-namespace Gendarme.Rules.Design {
+namespace Gendarme.Rules.Design
+{
+  /// <summary>
+  /// This rule ensure that <c>sealed</c> types (i.e. types that you can't inherit from)
+  /// do not define new <c>virtual</c> methods. Such methods would only be useful in
+  /// sub-types. Note that some compilers, like C# and VB.NET compilers, do not allow you
+  /// to define such methods.
+  /// </summary>
+  /// <example>
+  /// Bad example:
+  /// <code>
+  /// public sealed class MyClass {
+  ///	// note that C# compilers won't allow this to compile
+  ///	public virtual int GetAnswer ()
+  ///	{
+  ///		return 42;
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
+  /// <example>
+  /// Good example:
+  /// <code>
+  /// public sealed class MyClass {
+  ///	public int GetAnswer ()
+  ///	{
+  ///		return 42;
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
 
-	/// <summary>
-	/// This rule ensure that <c>sealed</c> types (i.e. types that you can't inherit from) 
-	/// do not define new <c>virtual</c> methods. Such methods would only be useful in
-	/// sub-types. Note that some compilers, like C# and VB.NET compilers, do not allow you
-	/// to define such methods.
-	/// </summary>
-	/// <example>
-	/// Bad example:
-	/// <code>
-	/// public sealed class MyClass {
-	///	// note that C# compilers won't allow this to compile
-	///	public virtual int GetAnswer ()
-	///	{
-	///		return 42;
-	///	}
-	/// }
-	/// </code>
-	/// </example>
-	/// <example>
-	/// Good example:
-	/// <code>
-	/// public sealed class MyClass {
-	///	public int GetAnswer ()
-	///	{
-	///		return 42;
-	///	}
-	/// }
-	/// </code>
-	/// </example>
+  [Problem("This sealed type introduces new virtual methods.")]
+  [Solution("Change the accessibility to public or private to represent its true intended use.")]
+  [FxCopCompatibility("Microsoft.Design", "CA1048:DoNotDeclareVirtualMembersInSealedTypes")]
+  public class DoNotDeclareVirtualMethodsInSealedTypeRule : Rule, ITypeRule
+  {
+    public RuleResult CheckType(TypeDefinition type)
+    {
+      // rule applies only to sealed types, but not to enum, value types and delegate (sealed)
+      if (!type.IsSealed || type.IsEnum || type.IsValueType || type.IsDelegate())
+        return RuleResult.DoesNotApply;
 
-	[Problem ("This sealed type introduces new virtual methods.")]
-	[Solution ("Change the accessibility to public or private to represent its true intended use.")]
-	[FxCopCompatibility ("Microsoft.Design", "CA1048:DoNotDeclareVirtualMembersInSealedTypes")]
-	public class DoNotDeclareVirtualMethodsInSealedTypeRule : Rule, ITypeRule {
+      // F# compiler-generated types blithely ignore these rules
+      if (type.Name.Contains("@"))
+        return RuleResult.DoesNotApply;
 
-		public RuleResult CheckType (TypeDefinition type)
-		{
-			// rule applies only to sealed types, but not to enum, value types and delegate (sealed)
-			if (!type.IsSealed || type.IsEnum || type.IsValueType || type.IsDelegate ())
-				return RuleResult.DoesNotApply;
+      foreach (MethodDefinition method in type.Methods)
+      {
+        // method is virtual and not final (sealed)
+        if (method.IsVirtual && !method.IsFinal)
+        {
+          // so just make sure it's not an override of an ancestor
+          if (!method.IsOverride())
+            Runner.Report(method, Severity.Low, Confidence.Total);
+        }
+      }
 
-			foreach (MethodDefinition method in type.Methods) {
-				// method is virtual and not final (sealed)
-				if (method.IsVirtual && !method.IsFinal) {
-					// so just make sure it's not an override of an ancestor
-					if (!method.IsOverride ())
-						Runner.Report (method, Severity.Low, Confidence.Total);
-				}
-			}
-
-			return Runner.CurrentRuleResult;
-		}
-	}
+      return Runner.CurrentRuleResult;
+    }
+  }
 }
