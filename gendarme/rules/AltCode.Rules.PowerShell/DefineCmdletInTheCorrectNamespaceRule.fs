d303fd09d3296fcce95f9a6f59970cdfa2c1c93d
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-01-21 13:27:33 +0000

    Template rule+test

---
+++ gendarme/rules/AltCode.Rules.PowerShell/DefineCmdletInTheCorrectNamespaceRule.fs
@@ -0,0 +1,36 @@
+﻿namespace AltCode.Rules.PowerShell
+
+open System
+
+open Mono.Cecil
+open Mono.Cecil.Cil
+
+open Gendarme.Framework
+open Gendarme.Framework.Engines
+open Gendarme.Framework.Helpers
+open Gendarme.Framework.Rocks
+
+/// <summary>
+/// TODO: Add a summary of the rule.
+/// </summary>
+/// <example>
+/// Bad example:
+/// <code>
+/// TODO: Add an example where the rule would fail.
+/// </code>
+/// </example>
+/// <example>
+/// Good example:
+/// <code>
+/// TODO: Show how to fix the bad example.
+/// </code>
+/// </example>
+
+// TODO: Describe the problem and solution
+[<Problem ("")>]
+[<Solution ("")>]
+type DefineCmdletInTheCorrectNamespaceRule() =
+  inherit Rule()
+  interface ITypeRule with
+    member this.CheckType(``type`` : TypeDefinition) : RuleResult =
+      RuleResult.Success
---
+++ gendarme/rules/AltCode.Rules.PowerShell/Tests.AltCode.PowerShell/DefineCmdletInTheCorrectNamespaceRule.fs
@@ -0,0 +1,32 @@
+﻿namespace Tests.AltCode.PowerShell
+
+open System
+open System.Reflection
+
+open Mono.Cecil
+// Add open for the project of the rule being tested.
+open AltCode.Rules.PowerShell
+
+open NUnit.Framework
+open Test.Rules.Fixtures
+open Test.Rules.Helpers
+open Test.Rules.Definitions
+
+[<TestFixture>]
+type DefineCmdletInTheCorrectNamespaceRule() =
+  inherit TypeRuleTestFixture<AltCode.Rules.PowerShell.DefineCmdletInTheCorrectNamespaceRule>()
+
+  [<Test>]
+  member this.DoesNotApply () =
+    // AssertRuleDoesNotApply<type> ()
+    ()
+
+  [<Test>]
+  member this.Good () =
+    // AssertRuleSuccess<type> ()
+    ()
+
+  [<Test>]
+  member this.Bad () =
+    // AssertRuleFailure<type> ()
+    ()
