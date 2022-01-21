4db5eb2bc5b721d7e2651444e1e0e30c18531c31
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-01-21 14:47:39 +0000

    Skeleton for this

--- gendarme/rules/AltCode.Rules.General/JustifySuppressionRule.fs
+++ gendarme/rules/AltCode.Rules.General/JustifySuppressionRule.fs
@@ -1,4 +1,4 @@
-﻿namespace AltCode.Rules.PowerShell
+﻿namespace AltCode.Rules.General
 
 open System
 
@@ -13,15 +13,20 @@ open Gendarme.Framework.Rocks
 [<Problem("The namespace in which the cmdlet class is defined must have a name in the following format: '<Product>.Commands'.>")>]
 [<Solution("Move or rename the class to a namespace that identifies the product and ends in '.Commands'.")>]
 [<Sealed>]
-type DefineCmdletInTheCorrectNamespaceRule() =
+type JustifySuppressionRule() =
   inherit Rule()
 
   interface ITypeRule with
     member this.CheckType(td: TypeDefinition) : RuleResult =
-      if Tools.IsCmdlet td then
-        if td.Namespace.EndsWith(".Commands", StringComparison.Ordinal) then
-          RuleResult.Success
-        else
-          RuleResult.Failure
-      else
         RuleResult.DoesNotApply
+
+  interface IMethodRule with
+    member this.CheckMethod(md: MethodDefinition) : RuleResult =
+        RuleResult.DoesNotApply
+
+  interface IAssemblyRule with
+    member this.CheckAssembly(ad: AssemblyDefinition) : RuleResult =
+        RuleResult.DoesNotApply
+
+  interface IRule with // keep compiler happy
+    member this.TearDown () = ()
---
+++ gendarme/rules/AltCode.Rules.General/Tests.AltCode.General/JustifySuppressionRule.fs
@@ -0,0 +1,70 @@
+﻿namespace Tests.AltCode.General
+
+open System
+open System.Reflection
+
+open Mono.Cecil
+// Add open for the project of the rule being tested.
+open AltCode.Rules.General
+
+open NUnit.Framework
+open Test.Rules.Fixtures
+open Test.Rules.Helpers
+open Test.Rules.Definitions
+
+[<TestFixture>]
+type TypeJustifySuppressionRule() =
+  inherit TypeRuleTestFixture<AltCode.Rules.General.JustifySuppressionRule>()
+
+  [<Test>]
+  member this.DoesNotApply() =
+    //base.AssertRuleDoesNotApply<DefineCmdletInTheCorrectNamespaceRule>
+    ()
+
+  [<Test>]
+  member this.Good() =
+    // AssertRuleSuccess<type> ()
+    ()
+
+  [<Test>]
+  member this.Bad() =
+    // AssertRuleFailure<type> ()
+    ()
+
+[<TestFixture>]
+type MethodJustifySuppressionRule() =
+  inherit MethodRuleTestFixture<AltCode.Rules.General.JustifySuppressionRule>()
+
+  [<Test>]
+  member this.DoesNotApply() =
+    //base.AssertRuleDoesNotApply<DefineCmdletInTheCorrectNamespaceRule>
+    ()
+
+  [<Test>]
+  member this.Good() =
+    // AssertRuleSuccess<type> ()
+    ()
+
+  [<Test>]
+  member this.Bad() =
+    // AssertRuleFailure<type> ()
+    ()
+
+[<TestFixture>]
+type AssemblyJustifySuppressionRule() =
+  inherit AssemblyRuleTestFixture<AltCode.Rules.General.JustifySuppressionRule>()
+
+  [<Test>]
+  member this.DoesNotApply() =
+    //base.AssertRuleDoesNotApply<DefineCmdletInTheCorrectNamespaceRule>
+    ()
+
+  [<Test>]
+  member this.Good() =
+    // AssertRuleSuccess<type> ()
+    ()
+
+  [<Test>]
+  member this.Bad() =
+    // AssertRuleFailure<type> ()
+    ()
