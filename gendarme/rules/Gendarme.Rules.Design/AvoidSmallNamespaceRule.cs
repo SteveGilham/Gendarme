681df94f246a70130f4b5591c0d2af22474f764a
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-02-07 18:19:25 +0000

    Reformatting

--- gendarme/rules/Gendarme.Rules.Design/AvoidSmallNamespaceRule.cs
+++ gendarme/rules/Gendarme.Rules.Design/AvoidSmallNamespaceRule.cs
@@ -38,141 +38,150 @@ using Gendarme.Framework.Engines;
 using Gendarme.Framework.Helpers;
 using Gendarme.Framework.Rocks;
 
-namespace Gendarme.Rules.Design {
+namespace Gendarme.Rules.Design
+{
+  /// <summary>
+  /// This rule fires if a namespace contains less than five (by default) visible types. Note
+  /// that this rule enumerates the types in all the assemblies being analyzed instead of
+  /// simply considering each assembly in turn.
+  /// The rule exempts:
+  /// <list>
+  /// <item><term>specialized namespaces</term><description>e.g. <c>*.Design</c>,
+  /// <c>*.Interop</c> and <c>*.Permissions</c></description></item>
+  /// <item><term>internal namespaces</term><description>namespaces without any visible
+  /// (outside the assemble) types</description></item>
+  /// <item><term>small assemblies</term><description>that contains a single namespace but less than
+  /// the minimal number of types (e.g. addins)</description></item>
+  /// <item><term>assembly entry point</term><description>the namespace of the type being
+  /// used in an assemble (EXE) entry-point</description></item>
+  /// </list>
+  /// </summary>
+  /// <example>
+  /// Bad example:
+  /// <code>
+  /// namespace MyStuff.Special {
+  ///	// single type inside a namespace
+  ///	public class Helper {
+  ///	}
+  /// }
+  /// </code>
+  /// </example>
+  /// <example>
+  /// Good example:
+  /// <code>
+  /// namespace MyStuff {
+  ///	public class Helper {
+  ///	}
+  ///	// ... many other types ...
+  /// }
+  /// </code>
+  /// </example>
 
-	/// <summary>
-	/// This rule fires if a namespace contains less than five (by default) visible types. Note
-	/// that this rule enumerates the types in all the assemblies being analyzed instead of
-	/// simply considering each assembly in turn.
-	/// The rule exempts:
-	/// <list>
-	/// <item><term>specialized namespaces</term><description>e.g. <c>*.Design</c>,
-	/// <c>*.Interop</c> and <c>*.Permissions</c></description></item>
-	/// <item><term>internal namespaces</term><description>namespaces without any visible 
-	/// (outside the assemble) types</description></item>
-	/// <item><term>small assemblies</term><description>that contains a single namespace but less than
-	/// the minimal number of types (e.g. addins)</description></item>
-	/// <item><term>assembly entry point</term><description>the namespace of the type being
-	/// used in an assemble (EXE) entry-point</description></item>
-	/// </list>
-	/// </summary>
-	/// <example>
-	/// Bad example:
-	/// <code>
-	/// namespace MyStuff.Special {
-	///	// single type inside a namespace
-	///	public class Helper {
-	///	}
-	/// }
-	/// </code>
-	/// </example>
-	/// <example>
-	/// Good example:
-	/// <code>
-	/// namespace MyStuff {
-	///	public class Helper {
-	///	}
-	///	// ... many other types ...
-	/// }
-	/// </code>
-	/// </example>
+  [Problem("This namespaces contains very few visible types.")]
+  [Solution("Consider merging this namespace with its parent or a sibling.")]
+  [EngineDependency(typeof(NamespaceEngine))]
+  [FxCopCompatibility("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes")]
+  public class AvoidSmallNamespaceRule : Rule, IAssemblyRule
+  {
+    private const int Default = 5;
+    private int minimum = Default;
 
-	[Problem ("This namespaces contains very few visible types.")]
-	[Solution ("Consider merging this namespace with its parent or a sibling.")]
-	[EngineDependency (typeof (NamespaceEngine))]
-	[FxCopCompatibility ("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes")]
-	public class AvoidSmallNamespaceRule : Rule, IAssemblyRule {
+    /// <summary>
+    /// The minimum number of types which must exist within a namespace.
+    /// </summary>
+    [DefaultValue(Default)]
+    [Description("The minimum of types that should exists inside a namespace.")]
+    public int Minimum
+    {
+      get { return minimum; }
+      set
+      {
+        if (value < 0)
+          throw new ArgumentOutOfRangeException("Minimum", "Must be positive");
+        minimum = value;
+      }
+    }
 
-		private const int Default = 5;
-		private int minimum = Default;
+    private HashSet<string> ignore = new HashSet<string>();
 
-		/// <summary>
-		/// The minimum number of types which must exist within a namespace.
-		/// </summary>
-		[DefaultValue (Default)]
-		[Description ("The minimum of types that should exists inside a namespace.")]
-		public int Minimum {
-			get { return minimum; }
-			set {
-				if (value < 0)
-					throw new ArgumentOutOfRangeException ("Minimum", "Must be positive");
-				minimum = value;
-			}
-		}
+    public RuleResult CheckAssembly(AssemblyDefinition assembly)
+    {
+      MethodDefinition entry_point = assembly.EntryPoint;
+      if (entry_point != null)
+      {
+        // for EXE assemblies ignore the namespace of the entry point
+        ignore.AddIfNew(entry_point.DeclaringType.GetTypeName().Namespace);
+      }
 
-		HashSet<string> ignore = new HashSet<string> ();
+      // ignore assemblies with a single namespace
+      int count = 0;
+      string single = null;
+      foreach (string ns in NamespaceEngine.NamespacesInside(assembly))
+      {
+        // no type inside the assembly
+        if (ns == null)
+          break;
 
-		public RuleResult CheckAssembly (AssemblyDefinition assembly)
-		{
-			MethodDefinition entry_point = assembly.EntryPoint;
-			if (entry_point != null) {
-				// for EXE assemblies ignore the namespace of the entry point
-                ignore.AddIfNew(entry_point.DeclaringType.GetTypeName().Namespace);
-			}
+        // only count if there are some visible types inside the namespace
+        bool visible = false;
+        foreach (TypeDefinition type in NamespaceEngine.TypesInside(ns))
+        {
+          if (type.IsVisible())
+          {
+            visible = true;
+            break;
+          }
+        }
 
-			// ignore assemblies with a single namespace
-			int count = 0;
-			string single = null;
-			foreach (string ns in NamespaceEngine.NamespacesInside (assembly)) {
-				// no type inside the assembly 
-				if (ns == null)
-					break;
+        if (visible)
+        {
+          single = ns;
+          count++;
+        }
 
-				// only count if there are some visible types inside the namespace
-				bool visible = false;
-				foreach (TypeDefinition type in NamespaceEngine.TypesInside (ns)) {
-					if (type.IsVisible ()) {
-						visible = true;
-						break;
-					}
-				}
+        // no need to go more than 2
+        if (count > 1)
+          break;
+      }
+      if (count == 1)
+        ignore.AddIfNew(single);
 
-				if (visible) {
-					single = ns;
-					count++;
-				}
+      return RuleResult.Success;
+    }
 
-				// no need to go more than 2
-				if (count > 1)
-					break;
-			}
-			if (count == 1)
-				ignore.AddIfNew (single);
+    public override void TearDown()
+    {
+      // check every namespaces inside the assembly set being analyzed
+      foreach (string ns in NamespaceEngine.AllNamespaces())
+      {
+        // skip specialized namespaces
+        if (NamespaceDefinition.IsSpecialized(ns))
+          continue;
 
-			return RuleResult.Success;
-		}
+        if (ignore.Contains(ns))
+          continue;
 
-		public override void TearDown ()
-		{
-			// check every namespaces inside the assembly set being analyzed
-			foreach (string ns in NamespaceEngine.AllNamespaces ()) {
+        int count = 0;
+        foreach (TypeDefinition type in NamespaceEngine.TypesInside(ns))
+        {
+          if (type.IsVisible())
+            count++;
+          // no early termination here so we can report the correct count
+        }
 
-				// skip specialized namespaces
-				if (NamespaceDefinition.IsSpecialized (ns))
-					continue;
-
-				if (ignore.Contains (ns))
-					continue;
-
-				int count = 0;
-				foreach (TypeDefinition type in NamespaceEngine.TypesInside (ns)) {
-					if (type.IsVisible ())
-						count++;
-					// no early termination here so we can report the correct count
-				}
-
-				// don't report namespaces that are not visible outside the assembly
-				// e.g. VS.NET adds a .Properties namespace to SWF apps
-				if ((count > 0) && (count < Minimum)) {
-					NamespaceDefinition n = NamespaceDefinition.GetDefinition (ns);
-					string msg = String.Format (CultureInfo.CurrentCulture, 
-						"Only {0} visible types are defined inside this namespace.", count);
-					// overloads of Report cannot be used here since the 'target' has been lost in the runner
-					Runner.Report (new Defect (this, n, n, Severity.Low, Confidence.Total, msg));
-				}
-			}
-			ignore.Clear ();
-			base.TearDown ();
-		}
-	}
+        // don't report namespaces that are not visible outside the assembly
+        // e.g. VS.NET adds a .Properties namespace to SWF apps
+        if ((count > 0) && (count < Minimum))
+        {
+          NamespaceDefinition n = NamespaceDefinition.GetDefinition(ns);
+          string msg = String.Format(CultureInfo.CurrentCulture,
+            "Only {0} visible types are defined inside this namespace.", count);
+          // overloads of Report cannot be used here since the 'target' has been lost in the runner
+          Runner.Report(new Defect(this, n, n, Severity.Low, Confidence.Total, msg));
+        }
+      }
+      ignore.Clear();
+      base.TearDown();
+    }
+  }
 }
