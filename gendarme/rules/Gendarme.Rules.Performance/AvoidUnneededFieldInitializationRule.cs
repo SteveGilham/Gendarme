14021d79e0fbdaeb07411df55ec530d02c3c4a1a
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-02-08 11:38:41 +0000

    Implement
    Commit: ec243b3c74cedc8af3dfca1c2031ad897d4382ce [ec243b3]
    Parents: 568366f294
    Author: JAD (SVK) <jad.1.svk+github@gmail.com>
    Date: 29 December 2017 13:08:42
    Committer: JAD (SVK)
    Fix reporting of unneeded initialization in generated code
    
    Visual Studio generates code for forms and/or user controls with
    initialization to default value. It is not a good idea to change GUI
    generated code, therefore it is better to hide this initialization from
    report list.

--- gendarme/rules/Gendarme.Rules.Performance/AvoidUnneededFieldInitializationRule.cs
+++ gendarme/rules/Gendarme.Rules.Performance/AvoidUnneededFieldInitializationRule.cs
@@ -126,12 +126,28 @@ namespace Gendarme.Rules.Performance
           // we're more confident about the unneeded initialization
           // on static ctor, since another (previous) ctor, can't set
           // the values differently
-          Confidence c = method.IsStatic ? Confidence.High : Confidence.Normal;
+          Confidence c;
+          if (method.IsStatic)
+          {
+            c = Confidence.High;
+          }
+          else
+          {
+            if (IsGuiGeneratedCode(method, fr))
+              return RuleResult.DoesNotApply;
+            c = Confidence.Normal;
+          }
           Runner.Report(method, ins, Severity.Medium, c, frName);
         }
       }
 
       return Runner.CurrentRuleResult;
+    }
+
+    private bool IsGuiGeneratedCode(MethodDefinition method, FieldReference fr)
+    {
+      return (string.Equals(fr.Name, "components", StringComparison.Ordinal)
+          && method.DeclaringType.IsDesignable());
     }
   }
 }
