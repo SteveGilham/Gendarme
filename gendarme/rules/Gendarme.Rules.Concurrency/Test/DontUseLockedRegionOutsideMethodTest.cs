3ab6c618d37ab01d13c55216d5e8a445ea3a2aec
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-02-09 15:24:54 +0000

    Commit: f4f0afa73c4dab237952649b1c3f4e876f89b363 [f4f0afa] Parents: c65869a8d1, 926d8d3d63 Author: JAD (SVK) <jad.1.svk+github@gmail.com> Date: 20 January 2018 16:19:31 Committer: JAD (SVK) Merge branch 'FixLockRegionOutside' into FixRules
    Fix text description for do not use lock outside region
    
    The original message and description were incorrect and confusing.
    
    When the code has a single <Enter> and <Exit> before each return,
    including <Exit> in <catch> block, the rule fires and the user may be
    really confused. Not to speaking of another mistake (see next commit).
    
    Fix rule 'do not use lock outside region' behaviour
    
    The original rule completely ignored the real meaning of Monitor class.
    The <Enter> method waits infinitely to enter and it can cause death
    lock. It is necessary to <TryEnter> and handle locked state.
    
    Fix example in  'do not use lock outside region'
    
    The original bad example used field name 'lock' which is a keyword
    and can not be used as a field name.
    
    At the same time, the example code formatting was fixed.
    
    Update 'Do not use lock outside' to skip generated code
    
    It has no meaning to check generated methods. Even when there is
    a problem, it can not be fixed. And usually these methods do not use
    lock (Monitor.Enter, Monitor.Exit) at all.
    
    Add check for use Monitor.Exit before Monitor.Enter
    
    To do functionality to check use of Monitor.Exit before Monitor.Enter
    was added. The code still do not check the difference between keyword
    lock and manually entered Monitor.Enter, Monitor.Exit functions.
    
    Additionally a nested lock test functionality was added.

--- gendarme/rules/Gendarme.Rules.Concurrency/Test/DontUseLockedRegionOutsideMethodTest.cs
+++ gendarme/rules/Gendarme.Rules.Concurrency/Test/DontUseLockedRegionOutsideMethodTest.cs
@@ -1,5 +1,5 @@
 //
-// Unit tests for DontUseLockedRegionOutsideMethodTest
+// Unit tests for DoNotUseLockedRegionOutsideMethodRule
 //
 // Authors:
 //	Andres G. Aragoneses <aaragoneses@novell.com>
@@ -13,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -26,70 +26,178 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+using System;
+using System.Threading;
 using Gendarme.Rules.Concurrency;
 
 using NUnit.Framework;
 using Test.Rules.Definitions;
 using Test.Rules.Fixtures;
 
-namespace Test.Rules.Concurrency {
+namespace Test.Rules.Concurrency
+{
+  [TestFixture]
+  public class DoNotUseLockedRegionOutsideMethodTest : MethodRuleTestFixture<DoNotUseLockedRegionOutsideMethodRule>
+  {
+    [Test]
+    public void DoesNotApply()
+    {
+      // no IL for p/invokes
+      AssertRuleDoesNotApply(SimpleMethods.ExternalMethod);
+      // no calls[virt]
+      AssertRuleDoesNotApply(SimpleMethods.EmptyMethod);
+    }
 
-	[TestFixture]
-	public class DoNotUseLockedRegionOutsideMethodTest : MethodRuleTestFixture<DoNotUseLockedRegionOutsideMethodRule> {
+    public class Monitors
+    {
+      private Monitors()
+      {
+      }
 
-		[Test]
-		public void DoesNotApply ()
-		{
-			// no IL for p/invokes
-			AssertRuleDoesNotApply (SimpleMethods.ExternalMethod);
-			// no calls[virt]
-			AssertRuleDoesNotApply (SimpleMethods.EmptyMethod);
-		}
+      public static void WithLockStatement()
+      {
+        lock (new object())
+        {
+          // do something...
+          WithLockStatement();
+        }
+      }
 
-		public class Monitors {
+      public static void WithoutConcurrency()
+      {
+        // do something...
+        WithoutConcurrency();
+      }
 
-			private Monitors ()
-			{
-			}
+      public static void WithoutThreadExit()
+      {
+        // do something...
+        WithoutThreadExit();
 
-			public static void WithLockStatement () {
-				lock ( new object () )
-				{
-					// do something...
-					WithLockStatement ();
-				}
-			}
+        System.Threading.Monitor.Enter(new object());
 
-			public static void WithoutConcurrency () {
-				// do something...
-				WithoutConcurrency ();
-			}
+        // do something...
+        WithoutThreadExit();
+      }
 
-			public static void WithoutThreadExit () {
-				// do something...
-				WithoutThreadExit ();
-				
-				System.Threading.Monitor.Enter ( new object () );
-				
-				// do something...
-				WithoutThreadExit ();
-			}
+      public static void TwoEnterOneExit()
+      {
+        lock (new object())
+        {
+          System.Threading.Monitor.Enter(new object());
+        }
+      }
+    }
 
-			public static void TwoEnterOneExit ()
-			{
-				lock (new object ()) {
-					System.Threading.Monitor.Enter (new object ());
-				}
-			}
-		}
-	
-		[Test]
-		public void Check ()
-		{
-			AssertRuleSuccess<Monitors> ("WithLockStatement");
-			AssertRuleSuccess<Monitors> ("WithoutConcurrency");
-			AssertRuleFailure<Monitors> ("WithoutThreadExit");
-			AssertRuleFailure<Monitors> ("TwoEnterOneExit");
-		}
-	}
+    public sealed class LockClass
+    {
+      private readonly object @lock = new object();
+      private readonly object mutex = new object();
+
+      public void Lock()
+      {
+        Monitor.Enter(@lock);
+      }
+
+      public void TryLock()
+      {
+        Monitor.TryEnter(mutex);
+      }
+
+      public void MultiLock()
+      {
+        Monitor.Enter(@lock);
+        Monitor.TryEnter(mutex);
+      }
+
+      public void Exit()
+      {
+        Monitor.Exit(@lock);
+      }
+
+      public void ExitLock()
+      {
+        Monitor.Exit(@lock);
+        Monitor.TryEnter(mutex);
+      }
+
+      public void LockExitDifferent()
+      {
+        Monitor.TryEnter(mutex);
+        Monitor.Exit(@lock);
+      }
+
+      public void LockExitOK1()
+      {
+        Monitor.TryEnter(mutex);
+        Monitor.Exit(mutex);
+      }
+
+      public void LockExitOK2()
+      {
+        Monitor.TryEnter(@lock);
+        Monitor.Exit(@lock);
+      }
+
+      public void MultipleLockExit()
+      {
+        Monitor.TryEnter(mutex);
+        Monitor.Exit(mutex);
+        Monitor.TryEnter(@lock);
+        Monitor.Exit(@lock);
+      }
+
+      public void LockExitExitLock()
+      {
+        Monitor.TryEnter(mutex);
+        Monitor.Exit(mutex);
+        Monitor.Exit(@lock);
+        Monitor.TryEnter(@lock);
+      }
+
+      public void NestedLock()
+      {
+        lock (@lock)
+        {
+          lock (mutex)
+            Console.WriteLine();
+        }
+      }
+    }
+
+    [Test]
+    public void Check()
+    {
+      AssertRuleSuccess<Monitors>("WithLockStatement");
+      AssertRuleSuccess<Monitors>("WithoutConcurrency");
+      AssertRuleFailure<Monitors>("WithoutThreadExit");
+      AssertRuleFailure<Monitors>("TwoEnterOneExit");
+    }
+
+    [Test]
+    public void UseLockCorrectly()
+    {
+      AssertRuleSuccess<LockClass>("LockExitOK1");
+      AssertRuleSuccess<LockClass>("LockExitOK2");
+    }
+
+    [Test]
+    public void IncorrectUseOfLock()
+    {
+      AssertRuleFailure<LockClass>("Lock");
+      AssertRuleFailure<LockClass>("TryLock");
+      AssertRuleFailure<LockClass>("MultiLock");
+      AssertRuleFailure<LockClass>("Exit");
+      AssertRuleFailure<LockClass>("ExitLock");
+      AssertRuleFailure<LockClass>("MultipleLockExit");
+      AssertRuleFailure<LockClass>("LockExit" + "ExitLock"); // spellcheck bypass
+      AssertRuleFailure<LockClass>("NestedLock");
+    }
+
+    [Test, Ignore("Not yet supported to test lock on different objects.")]
+    public void LockExitDifferent()
+    {
+      AssertRuleFailure<LockClass>("LockExitDifferent");
+    }
+  }
 }
