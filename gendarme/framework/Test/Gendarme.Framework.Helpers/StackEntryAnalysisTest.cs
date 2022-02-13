a726f5185c7455efb1bc22ed775b991f0730182d
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-02-13 23:47:52 +0000

    For the record -- get the same failure with the old .net4 compiler

--- gendarme/framework/Test/Gendarme.Framework.Helpers/StackEntryAnalysisTest.cs
+++ gendarme/framework/Test/Gendarme.Framework.Helpers/StackEntryAnalysisTest.cs
@@ -882,7 +882,7 @@ namespace Test.Framework
       using (var stream =
         Assembly
           .GetExecutingAssembly()
-          .GetManifestResourceStream("Test.Framework.MCSRetro.MCSRetro.dll"))
+          .GetManifestResourceStream("Test.Framework.csc2010.StackBadBoys.dll"))
       using (var ad = AssemblyDefinition.ReadAssembly(stream))
       {
         Console.WriteLine("Retro1");
@@ -903,7 +903,7 @@ namespace Test.Framework
       using (var stream =
         Assembly
           .GetExecutingAssembly()
-          .GetManifestResourceStream("Test.Framework.MCSRetro.MCSRetro.dll"))
+          .GetManifestResourceStream("Test.Framework.csc2010.StackBadBoys.dll"))
       using (var ad = AssemblyDefinition.ReadAssembly(stream))
       {
         Console.WriteLine("Retro2");
