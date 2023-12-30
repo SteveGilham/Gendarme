824ca3503f61ae9fc82a90248e2cdee39d620657
SteveGilham <SteveGilham@users.noreply.github.com>
2023-12-30 17:04:32 +0000

    Work rounf NUnit4 and netframework

--- Build/targets.fs
+++ Build/targets.fs
@@ -1711,7 +1711,7 @@ module Targets =
     _Target "JustUnitTest" JustUnitTest
     _Target "UnitTestDotNet" UnitTestDotNet
     _Target "Coverage" ignore
-    _Target "UnitTestWithAltCoverRunner" ignore // UnitTestWithAltCoverRunner
+    _Target "UnitTestWithAltCoverRunner" UnitTestWithAltCoverRunner
     _Target "UnitTestWithAltCoverCoreRunner" UnitTestWithAltCoverCoreRunner
     _Target "Packaging" Packaging
     _Target "OperationalTest" ignore
