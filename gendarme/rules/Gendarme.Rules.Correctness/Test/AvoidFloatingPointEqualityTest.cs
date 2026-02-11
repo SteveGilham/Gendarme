bb0bdb243cf108d2166415123b0e84af03c3072f
SteveGilham <SteveGilham@users.noreply.github.com>
2026-02-11 14:50:12 +0000

    * Update build tools (net10.0) and dependencies. * Make minimal fixes for System.ReadOnlySpan<> instances included by the compiler in string concation and string format checking

--- gendarme/rules/Gendarme.Rules.Correctness/Test/AvoidFloatingPointEqualityTest.cs
+++ gendarme/rules/Gendarme.Rules.Correctness/Test/AvoidFloatingPointEqualityTest.cs
@@ -36,335 +36,355 @@ using NUnit.Framework;
 using Test.Rules.Definitions;
 using Test.Rules.Fixtures;
 
-namespace Test.Rules.Correctness {
-
+namespace Test.Rules.Correctness
+{
 #pragma warning disable 168, 219
 
-	[TestFixture]
-	public class AvoidFloatingPointEqualityTest : MethodRuleTestFixture<AvoidFloatingPointEqualityRule> {
+  [TestFixture]
+  public class AvoidFloatingPointEqualityTest : MethodRuleTestFixture<AvoidFloatingPointEqualityRule>
+  {
+    [Test]
+    public void DoesNotApply()
+    {
+      AssertRuleDoesNotApply(SimpleMethods.ExternalMethod);
+      AssertRuleDoesNotApply(SimpleMethods.EmptyMethod);
+      AssertRuleDoesNotApply<float>();
+      AssertRuleDoesNotApply<double>();
+    }
 
-		[Test]
-		public void DoesNotApply ()
-		{
-			AssertRuleDoesNotApply (SimpleMethods.ExternalMethod);
-			AssertRuleDoesNotApply (SimpleMethods.EmptyMethod);
-			AssertRuleDoesNotApply<float> ();
-			AssertRuleDoesNotApply<double> ();
-		}
+    public class Double
+    {
+      public double field = 0.0d;
+      public static double staticField = 0.0d;
 
-		public class Double {
-			public double field = 0.0d;
-			public static double staticField = 0.0d;
+      public double Property
+      {
+        get { return @field; }
+      }
 
-			public double Property {
-				get { return field; }
-			}
+      public static double StaticMethod()
+      {
+        return 0.0d;
+      }
 
-			public static double StaticMethod ()
-			{
-				return 0.0d;
-			}
+      public void Fields()
+      {
+        bool a = field == staticField;
+        bool b = staticField == field;
+      }
 
-			public void Fields ()
-			{
-				bool a = field == staticField;
-				bool b = staticField == field;
-			}
+      public bool MethodResult()
+      {
+        bool a = Property == StaticMethod();
+        bool b = StaticMethod() == Property;
+        return a && b;
+      }
 
-			public bool MethodResult ()
-			{
-				bool a = Property == StaticMethod ();
-				bool b = StaticMethod () == Property;
-				return a && b;
-			}
+      public void Parameters(double f, double g)
+      {
+        bool a = f == g;
+      }
 
-			public void Parameters (double f, double g)
-			{
-				bool a = f == g;
-			}
+      public bool StaticParameters(double f, double g)
+      {
+        return f == g;
+      }
 
-			public bool StaticParameters (double f, double g)
-			{
-				return f == g;
-			}
+      public void Local()
+      {
+        double d1 = 0.0d;
+        double d2 = 0.0d;
+        if (d1 == d2)
+        {
+          int i = 1;
+          Console.WriteLine(d1 == (double)i);
+        }
+        else if (d2 == d1)
+        {
+          uint i = 1;
+          Console.WriteLine((double)i == d2);
+        }
+      }
 
-			public void Local ()
-			{
-				double d1 = 0.0d;
-				double d2 = 0.0d;
-				if (d1 == d2) {
-					int i = 1;
-					Console.WriteLine (d1 == (double)i);
-				} else if (d2 == d1) {
-					uint i = 1;
-					Console.WriteLine ((double)i == d2);
-				}
-			}
+      public void LocalAbove4()
+      {
+        string s1 = String.Empty;
+        string s2 = String.Empty;
+        string s3 = String.Empty;
+        string s4 = String.Empty;
+        double d1 = 0.0d;
+        double d2 = 0.0d;
+        if (d1 == d2)
+        {
+          Console.WriteLine(d1 == ((double)1));
+        }
+        else if (d2 == d1)
+        {
+          Console.WriteLine(((double)0) == d2);
+        }
+      }
 
-			public void LocalAbove4 ()
-			{
-				string s1 = String.Empty;
-				string s2 = String.Empty;
-				string s3 = String.Empty;
-				string s4 = String.Empty;
-				double d1 = 0.0d;
-				double d2 = 0.0d;
-				if (d1 == d2) {
-					Console.WriteLine (d1 == ((double)1));
-				} else if (d2 == d1) {
-					Console.WriteLine (((double)0) == d2);
-				}
-			}
+      public void ComparisonAfterArithmeticOperation()
+      {
+        double d1 = 0.0d;
+        double d2 = 0.0d;
+        bool a = d1 * 1 == d2 * 1;
+      }
 
-			public void ComparisonAfterArithmeticOperation ()
-			{
-				double d1 = 0.0d;
-				double d2 = 0.0d;
-				bool a = d1 * 1 == d2 * 1;
-			}
+      public void LegalComparisons()
+      {
+        double d = 0.0d;
+        bool a;
+        bool b;
 
-			public void LegalComparisons ()
-			{
-				double d = 0.0d;
-				bool a;
-				bool b;
+        //a = float.PositiveInfinity == f;
+        b = d == double.PositiveInfinity;
 
-				//a = float.PositiveInfinity == f;
-				b = d == double.PositiveInfinity;
+        //a = float.NegativeInfinity == f;
+        b = d == double.NegativeInfinity;
+      }
 
-				//a = float.NegativeInfinity == f;
-				b = d == double.NegativeInfinity;
-			}
+      public void EqualsCall()
+      {
+        double d1 = 0.0d;
+        double d2 = 0.0d;
+        if (d1.Equals(d2))
+        {
+          d1.Equals(1.0);
+        }
+        else
+        {
+          (1.0d).Equals(d2);
+        }
+      }
 
-			public void EqualsCall ()
-			{
-				double d1 = 0.0d;
-				double d2 = 0.0d;
-				if (d1.Equals (d2)) {
-					d1.Equals (1.0);
-				} else {
-					(1.0d).Equals (d2);
-				}
-			}
+      public bool CompareWithArray()
+      {
+        double[] values = new double[] { 0.0d, 1.0d };
+        return values[0] == values[1];
+      }
+    }
 
-			public bool CompareWithArray ()
-			{
-				double [] values = new double [] { 0.0d, 1.0d };
-				return values [0] == values [1];
-			}
-		}
+    public class Float
+    {
+      public float field = 0f;
+      public static float staticField = 0f;
 
-		public class Float {
+      public float Property
+      {
+        get { return @field; }
+      }
 
-			public float field = 0f;
-			public static float staticField = 0f;
+      public static float StaticMethod()
+      {
+        return 0f;
+      }
 
-			public float Property {
-				get { return field; }
-			}
+      public void Fields()
+      {
+        bool a = field == staticField;
+        bool b = staticField == field;
+      }
 
-			public static float StaticMethod ()
-			{
-				return 0f;
-			}
+      public bool MethodResult()
+      {
+        bool a = Property == StaticMethod();
+        bool b = StaticMethod() == Property;
+        return a && b;
+      }
 
-			public void Fields ()
-			{
-				bool a = field == staticField;
-				bool b = staticField == field;
-			}
+      public void Parameters(float f, float g)
+      {
+        bool a = f == g;
+      }
 
-			public bool MethodResult ()
-			{
-				bool a = Property == StaticMethod();
-				bool b = StaticMethod() == Property;
-				return a && b;
-			}
+      public bool StaticParameters(float f, float g)
+      {
+        return f == g;
+      }
 
-			public void Parameters (float f, float g)
-			{
-				bool a = f == g;
-			}
+      public void Local()
+      {
+        float f1 = 0f;
+        float f2 = 0f;
+        if (f1 == f2)
+        {
+          int i = 1;
+          Console.WriteLine(f1 == (float)i);
+        }
+        else if (f2 == f1)
+        {
+          uint i = 1;
+          Console.WriteLine((float)i == f2);
+        }
+      }
 
-			public bool StaticParameters (float f, float g)
-			{
-				return f == g;
-			}
+      public void LocalAbove4()
+      {
+        string s1 = String.Empty;
+        string s2 = String.Empty;
+        string s3 = String.Empty;
+        string s4 = String.Empty;
+        float f1 = 0f;
+        float f2 = 0f;
+        if (f1 == f2)
+        {
+          Console.WriteLine(f1 == ((float)1));
+        }
+        else if (f2 == f1)
+        {
+          Console.WriteLine(((float)0) == f2);
+        }
+      }
 
-			public void Local ()
-			{
-				float f1 = 0f;
-				float f2 = 0f;
-				if (f1 == f2) {
-					int i = 1;
-					Console.WriteLine (f1 == (float) i);
-				} else if (f2 == f1) {
-					uint i = 1;
-					Console.WriteLine ((float) i == f2);
-				}
-			}
+      public void ComparisonAfterArithmeticOperation()
+      {
+        float f1 = 0f;
+        float f2 = 0f;
+        bool a = f1 * 1 == f2 * 1;
+      }
 
-			public void LocalAbove4 ()
-			{
-				string s1 = String.Empty;
-				string s2 = String.Empty;
-				string s3 = String.Empty;
-				string s4 = String.Empty;
-				float f1 = 0f;
-				float f2 = 0f;
-				if (f1 == f2) {
-					Console.WriteLine (f1 == ((float) 1));
-				} else if (f2 == f1) {
-					Console.WriteLine (((float) 0) == f2);
-				}
-			}
+      public void LegalComparisons()
+      {
+        float f = 0f;
+        bool a;
+        bool b;
 
-			public void ComparisonAfterArithmeticOperation ()
-			{
-				float f1 = 0f;
-				float f2 = 0f;
-				bool a = f1 * 1 == f2 * 1;
-			}
+        //a = float.PositiveInfinity == f;
+        b = f == float.PositiveInfinity;
 
-			public void LegalComparisons ()
-			{
-				float f = 0f;
-				bool a;
-				bool b;
-				
-				//a = float.PositiveInfinity == f;
-				b = f == float.PositiveInfinity;
-				
-				//a = float.NegativeInfinity == f;
-				b = f == float.NegativeInfinity;
-			}
+        //a = float.NegativeInfinity == f;
+        b = f == float.NegativeInfinity;
+      }
 
-			public void EqualsCall ()
-			{
-				float f1 = 0f;
-				float f2 = 0f;
-				bool a = f1.Equals(f2);
-			}
+      public void EqualsCall()
+      {
+        float f1 = 0f;
+        float f2 = 0f;
+        bool a = f1.Equals(f2);
+      }
 
-			public bool CompareWithArray ()
-			{
-				float [] values = new float [] { 0.0f, 1.0f };
-				return values [0] == values [1];
-			}
-		}
+      public bool CompareWithArray()
+      {
+        float[] values = new float[] { 0.0f, 1.0f };
+        return values[0] == values[1];
+      }
+    }
 
-		public class Legal {
+    public class Legal
+    {
+      private object o = null;
 
-			object o = null;
+      public void IntegerComparisons()
+      {
+        int i = 0;
+        int j = 1;
+        bool a = i == j;
+      }
 
-			public void IntegerComparisons ()
-			{
-				int i = 0;
-				int j = 1;
-				bool a = i == j;
-			}
+      public bool NoComparison()
+      {
+        return false;
+      }
 
-			public bool NoComparison ()
-			{
-				return false;
-			}
+      public bool Property
+      {
+        get
+        {
+          if (o != null)
+            return ((object)o.ToString() == this);
+          else
+            return false;
+        }
+      }
+    }
 
-			public bool Property {
-				get {
-					if (o != null)
-						return ((object)o.ToString () == this);
-					else
-						return false;
-				}
-			}
-		}
+    [Test]
+    public void TestFields()
+    {
+      AssertRuleFailure<Float>("Fields", 2);
+      AssertRuleFailure<Double>("Fields", 2);
+    }
 
-		[Test]
-		public void TestFields ()
-		{
-			AssertRuleFailure<Float> ("Fields", 2);
-			AssertRuleFailure<Double> ("Fields", 2);
-		}
+    [Test]
+    public void TestMethodResult()
+    {
+      AssertRuleFailure<Float>("MethodResult", 2);
+      AssertRuleFailure<Double>("MethodResult", 2);
+    }
 
-		[Test]
-		public void TestMethodResult ()
-		{
-			AssertRuleFailure<Float> ("MethodResult", 2);
-			AssertRuleFailure<Double> ("MethodResult", 2);
-		}
-		
-		[Test]
-		public void TestParameters ()
-		{
-			AssertRuleFailure<Float> ("Parameters", 1);
-			AssertRuleFailure<Double> ("Parameters", 1);
-		}
+    [Test]
+    public void TestParameters()
+    {
+      AssertRuleFailure<Float>("Parameters", 1);
+      AssertRuleFailure<Double>("Parameters", 1);
+    }
 
-		[Test]
-		public void TestStaticParameters ()
-		{
-			AssertRuleFailure<Float> ("StaticParameters", 1);
-			AssertRuleFailure<Double> ("StaticParameters", 1);
-		}
+    [Test]
+    public void TestStaticParameters()
+    {
+      AssertRuleFailure<Float>("StaticParameters", 1);
+      AssertRuleFailure<Double>("StaticParameters", 1);
+    }
 
-		[Test]
-		public void TestLocal ()
-		{
-			AssertRuleFailure<Float> ("Local");
-			AssertRuleFailure<Double> ("Local");
-		}
+    [Test]
+    public void TestLocal()
+    {
+      AssertRuleFailure<Float>("Local");
+      AssertRuleFailure<Double>("Local");
+    }
 
-		[Test]
-		public void TestLocalAbove4 ()
-		{
-			AssertRuleFailure<Float> ("LocalAbove4");
-			AssertRuleFailure<Double> ("LocalAbove4");
-		}
+    [Test]
+    public void TestLocalAbove4()
+    {
+      AssertRuleFailure<Float>("LocalAbove4");
+      AssertRuleFailure<Double>("LocalAbove4");
+    }
 
-		[Test]
-		public void TestComparisonAfterArithmeticOperation ()
-		{
-			AssertRuleFailure<Float> ("ComparisonAfterArithmeticOperation", 1);
-			AssertRuleFailure<Double> ("ComparisonAfterArithmeticOperation", 1);
-		}
+    [Test]
+    public void TestComparisonAfterArithmeticOperation()
+    {
+      AssertRuleFailure<Float>("ComparisonAfterArithmeticOperation", 1);
+      AssertRuleFailure<Double>("ComparisonAfterArithmeticOperation", 1);
+    }
 
-		[Test]
-		public void TestLegalComparisons ()
-		{
-			AssertRuleSuccess<Float> ("LegalComparisons");
-			AssertRuleSuccess<Double> ("LegalComparisons");
-		}
+    [Test]
+    public void TestLegalComparisons()
+    {
+      AssertRuleSuccess<Float>("LegalComparisons");
+      AssertRuleSuccess<Double>("LegalComparisons");
+    }
 
-		[Test]
-		public void TestLegalIntegerComparisons ()
-		{
-			AssertRuleSuccess<Legal> ("IntegerComparisons");
-		}
+    [Test]
+    public void TestLegalIntegerComparisons()
+    {
+      AssertRuleSuccess<Legal>("IntegerComparisons");
+    }
 
-		[Test]
-		public void TestEqualsCall ()
-		{
-			AssertRuleFailure<Float> ("EqualsCall", 1);
-			AssertRuleFailure<Double> ("EqualsCall", 3);
-		}
-		
-		[Test]
-		public void TestNoFloatComparison()
-		{
-			AssertRuleDoesNotApply<Legal> ("NoComparison");
-		}
+    [Test]
+    public void TestEqualsCall()
+    {
+      AssertRuleFailure<Float>("EqualsCall", 1);
+      AssertRuleFailure<Double>("EqualsCall", 3);
+    }
 
-		[Test]
-		public void TestCompareWithArray ()
-		{
-			AssertRuleFailure<Float> ("CompareWithArray", 1);
-			AssertRuleFailure<Double> ("CompareWithArray", 1);
-		}
+    [Test]
+    public void TestNoFloatComparison()
+    {
+      AssertRuleDoesNotApply<Legal>("NoComparison");
+    }
 
-		[Test]
-		public void TestLegalStuff ()
-		{
-			AssertRuleSuccess<Legal> ("get_Property");
-		}
-	}
+    [Test]
+    public void TestCompareWithArray()
+    {
+      AssertRuleFailure<Float>("CompareWithArray", 1);
+      AssertRuleFailure<Double>("CompareWithArray", 1);
+    }
+
+    [Test]
+    public void TestLegalStuff()
+    {
+      AssertRuleSuccess<Legal>("get_Property");
+    }
+  }
 }
